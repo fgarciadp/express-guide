6dcabe830a7b67123276256753401ec0710e5f70
Felipe Garcia <22felipe95@gmail.com>
2023-10-21 18:29:43 -0400

    added the route to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -13,6 +14,9 @@ app.use(express.json());
 app.use('/campsites', campsiteRouter);
 // Task 2 - integrate promotions nodule
 app.use('promotions', promotionRouter);
+// Task 3 - integrate partners nodule;
+app.use('partners', partnerRouter);
+
 
 app.use(express.static(__dirname + '/public'));
 
