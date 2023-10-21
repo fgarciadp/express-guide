1404fe7303a31fe82e7528486b82be3b5b6dd8d3
Felipe Garcia <22felipe95@gmail.com>
2023-10-21 18:27:43 -0400

    added partner and promotions route

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,26 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+// Task 1 - replicate the route but for specific campsite IDs. We're using params since we're retrieving resources through GET and PUT and DELETE
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /campsites/:campsiteId');
+})
+.put((req, res) => {
+    res.end(`Will update the campsite: ${req.params.campsiteId} with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Will delete the campsite: ${req.params.campsiteId}`);
+});
+
+
 module.exports = campsiteRouter;
